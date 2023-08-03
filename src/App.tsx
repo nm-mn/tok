fcd05846226e87fb04f40f26d0c085cda01abc26
nm-mn <nm.program.mn@gmail.com>
2023-08-04 00:06:00 +0900

    refactor: remove unused auth code

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,10 @@ import NavBar from "./common/NavBar"
 import ProfileList from './profile/ProfileList';
 import ProfileDetail from './profile/ProfileDetail';
 import ProtectedRoute from "./ProtectedRoute";
-import { User } from "./auth/useUser";
-import { useAuth } from "./auth/useAuth"
 import useAuthenticated from './hooks/useAuthenticated';
 import { LOCALSTORAGE_KEY_AUTHENTICATION } from "./constants/internal_common"
 import Inbox from "./inbox/Inbox"
 
-export const AuthContext = createContext({
-  user: null,
-  setUser: (user: User | null) => { }
-});
 
 export const AuthenticatedContext = createContext({
   // isAuthenticated: localStorage.getItem(LOCALSTORAGE_KEY_AUTHENTICATION).toLowerCase() === "true",
@@ -26,30 +20,27 @@ export const AuthenticatedContext = createContext({
 });
 
 function App() {
-  const { user, login, logout } = useAuth();
 
   return (
     <ThemeProvider theme={theme}>
       <AuthenticatedContext.Provider value={useAuthenticated()}>
-        {/* <AuthContext.Provider value={{ user, setUser }}> */}
-          <NavBar />
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={
-              <ProtectedRoute>
-                <ProfileList />
-              </ProtectedRoute>
-            } />
-            <Route path="/profile/:id" element={
-              <ProtectedRoute>
-                <ProfileDetail />
-              </ProtectedRoute>} />
-            <Route path="/inbox" element={
-              <ProtectedRoute>
-                <Inbox />
-              </ProtectedRoute>} />
-          </Routes>
-          {/* </AuthContext.Provider> */}
+        <NavBar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={
+            <ProtectedRoute>
+              <ProfileList />
+            </ProtectedRoute>
+          } />
+          <Route path="/profile/:id" element={
+            <ProtectedRoute>
+              <ProfileDetail />
+            </ProtectedRoute>} />
+          <Route path="/inbox" element={
+            <ProtectedRoute>
+              <Inbox />
+            </ProtectedRoute>} />
+        </Routes>
       </AuthenticatedContext.Provider>
     </ThemeProvider>
   );
